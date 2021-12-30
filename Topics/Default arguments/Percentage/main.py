d2daa13aee73e0f96b1f45730a4c293b09282fcf
liuyunan <liuyunan.20@gmail.com>
2021-12-29 21:45:46 -0800

    the default value of the second argument of round() function is None

--- Topics/Default arguments/Percentage/main.py
+++ Topics/Default arguments/Percentage/main.py
@@ -1,5 +1,2 @@
 def get_percentage(num, round_digits=None):
-    if round_digits is None:
-        return f"{round(num * 100)}%"
-    else:
-        return f"{round(num * 100, round_digits)}%"
+    return f"{round(num * 100, round_digits)}%"
