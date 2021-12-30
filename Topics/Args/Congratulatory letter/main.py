0dfb147163c5b60e36faa4e6bd0a6a168152d026
liuyunan <liuyunan.20@gmail.com>
2021-12-29 23:45:07 -0800

    Define a function congratulations() that lists the names of everyone in the letter.

---
+++ Topics/Args/Congratulatory letter/main.py
@@ -0,0 +1,7 @@
+def congratulations(pm, tester, *dps):
+    print('''Happy New Year! Take care of yourself and your loved ones!
+For:''')
+    print(pm)
+    print(tester)
+    for dp in dps:
+        print(dp)
