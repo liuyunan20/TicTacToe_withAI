b2b651a5cce1220c2cecb07f5c71338d42cb286e
liuyunan <liuyunan.20@gmail.com>
2022-01-01 00:45:22 -0800

    build a class TicTacToe to perform the game

--- Tic-Tac-Toe with AI/task/tictactoe.py
+++ Tic-Tac-Toe with AI/task/tictactoe.py
@@ -55,16 +55,16 @@ def process_input():
             if user_in_1 > 3 or user_in_1 < 1 or user_in_2 > 3 or user_in_2 < 1:
                 print("Coordinates should be from 1 to 3!")
             else:
-                return [user_in_1, user_in_2]
+                return [user_in_1 - 1, user_in_2 - 1]
 
 
-def check_blank_position(grids, co):
-    x = co[0]
-    y = co[1]
-    if grids[x - 1][y - 1] == "_":
-        return True
-    else:
-        return False
+# def check_blank_position(grids, co):
+#     x = co[0]
+#     y = co[1]
+#     if grids[x - 1][y - 1] == "_":
+#         return True
+#     else:
+#         return False
 
 
 def check_full(grids):
@@ -75,45 +75,61 @@ def check_full(grids):
     return True
 
 
-def check_part(grids):
-    x_num = 0
-    o_num = 0
-    for x in range(3):
-        for y in range(3):
-            if grids[x][y] == "X":
-                x_num += 1
-            elif grids[x][y] == "O":
-                o_num += 1
-    if x_num == o_num:
-        return "X"
-    elif x_num > o_num:
-        return "O"
+# def check_part(grids):
+#     x_num = 0
+#     o_num = 0
+#     for x in range(3):
+#         for y in range(3):
+#             if grids[x][y] == "X":
+#                 x_num += 1
+#             elif grids[x][y] == "O":
+#                 o_num += 1
+#     if x_num == o_num:
+#         return "X"
+#     elif x_num > o_num:
+#         return "O"
 
 
-def user_make_move(grids, co):
-    while True:
-        x = co[0]
-        y = co[1]
-
-        if check_blank_position(grids, co):
-            grids[x - 1][y - 1] = "X"
-            return grids
-        else:
-            print("This cell is occupied! Choose another one!")
-            co = process_input()
+def make_move(grids, co, part):
+    x = co[0]
+    y = co[1]
+    if co in find_blank_positions(grids):
+        grids[x][y] = part
 
 
-def computer_move_easy(grids):
+# def user_make_move(grids, co):
+#     while True:
+#         x = co[0]
+#         y = co[1]
+#
+#         if check_blank_position(grids, co):
+#             grids[x - 1][y - 1] = "X"
+#             return grids
+#         else:
+#             print("This cell is occupied! Choose another one!")
+#             co = process_input()
+
+
+def find_blank_positions(grids):
     blank_positions = []
     for x in range(3):
         for y in range(3):
             if grids[x][y] == "_":
                 blank_positions.append([x, y])
-    p = random.choice(blank_positions)
-    x = p[0]
-    y = p[1]
-    grids[x][y] = "O"
-    return grids
+    return blank_positions
+
+
+# def computer_move_easy(grids):
+#     blank_positions = []
+#     for x in range(3):
+#         for y in range(3):
+#             if grids[x][y] == "_":
+#                 blank_positions.append([x, y])
+#     p = random.choice(blank_positions)
+#     x = p[0]
+#     y = p[1]
+#     grids[x][y] = "O"
+#     return grids
 
 
 def check_end(grids):
@@ -130,13 +146,13 @@ def menu():
         user_in = input().split()
         if user_in[0] == "start":
             try:
-                x_part = user_in[1]
-                o_part = user_in[2]
+                x = user_in[1]
+                o = user_in[2]
             except IndexError:
                 print("Bad parameters!")
             else:
-                if x_part in ["easy", "user"] and o_part in ["easy", "user"]:
-                    return [x_part, o_part]
+                if x in ["easy", "user"] and o in ["easy", "user"]:
+                    return [x, o]
                 else:
                     print("Bad parameters!")
         elif user_in[0] == "exit":
@@ -145,20 +161,64 @@ def menu():
             print("Bad parameters!")
 
 
-grid = initial_grids("_" * 9)
-print_grids(grid)
-while True:
-    if check_end(grid):
-        print(check_end(grid))
-        break
-    else:
-        if check_part(grid) == "X":
+class TicTacToe:
+    grid = initial_grids("_" * 9)
+
+    def __init__(self, user_type, user_part):
+        self.user_type = user_type
+        self.user_part = user_part
+
+    def game(self):
+        blank_positions = find_blank_positions(self.grid)
+        if self.user_type == "user":
             coordinates = process_input()
-            grid = user_make_move(grid, coordinates)
-        elif check_part(grid) == "O":
-            grid = computer_move_easy(grid)
-            print('Making move level "easy"')
-        print_grids(grid)
+            while True:
+                if coordinates in blank_positions:
+                    make_move(self.grid, coordinates, self.user_part)
+                    return self.grid
+                else:
+                    print("This cell is occupied! Choose another one!")
+                    coordinates = process_input()
+        elif self.user_type == "easy":
+            coordinates = random.choice(blank_positions)
+            make_move(self.grid, coordinates, self.user_part)
+            return self.grid
+        else:
+            print("Bad parameters!")
+
+
+
+    # while True:
+    #     if check_end(grid):
+    #         print(check_end(grid))
+    #         break
+    #     else:
+    #
+    #         if check_part(grid) == "X":
+    #             coordinates = process_input()
+    #             grid = user_make_move(grid, coordinates)
+    #         elif check_part(grid) == "O":
+    #             grid = computer_move_easy(grid)
+    #             print('Making move level "easy"')
+    #         print_grids(grid)
+
+
+
+if menu():
+    parts = menu()
+    game
+    while True:
+        if check_end(grid):
+            print(check_end(grid))
+            break
+        else:
+            if check_part(grid) == "X":
+                coordinates = process_input()
+                grid = user_make_move(grid, coordinates)
+            elif check_part(grid) == "O":
+                grid = computer_move_easy(grid)
+                print('Making move level "easy"')
+            print_grids(grid)
 
 
 
